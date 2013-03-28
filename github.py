55497a130b5207faf806b49362369569a7f629c8
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-28 06:18:33 -0500

    Fetch list of user's repos with client.repo_names()

--- github.py
+++ github.py
@@ -1,7 +1,23 @@
+import requests
+import json
+
 class Client:
   def __init__(self, username, token):
     self.username = username
     self.token = token
+
+  def repo_names(self):
+    return [repo['full_name'] for repo in self.repositories()]
+
+  def repositories(self):
+    return self.perform_request("user/repos")
+
+  def request_url(self, action):
+    return "https://api.github.com/%s?access_token=%s" % (action, self.token)
+
+  def perform_request(self, action):
+    return json.loads(requests.get(self.request_url(action)).text)
+
 
 
 
