57b9f9a131153b93b80e00efc25d472b7ba47566
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-28 07:31:50 -0500

    add some pydoc

--- github.py
+++ github.py
@@ -1,3 +1,9 @@
+"""Fetch information from GitHub through its API
+
+This is an intentionally simple GitHub API client, built as much for
+experimentation and learning as it is for functionality. Your mileage may vary.
+"""
+
 import requests
 import json
 
@@ -7,17 +13,18 @@ class Client:
     self.token = token
 
   def repo_names(self):
+    """Array of names of repositories you have access to"""
     return [repo['full_name'] for repo in self.repositories()]
 
   def repositories(self):
+    """Array of Dicts of information about repositores you have access to"""
     return self.perform_request("user/repos")
 
   def request_url(self, action):
+    """URL to request information from the GitHub API"""
     return "https://api.github.com/%s?access_token=%s" % (action, self.token)
 
   def perform_request(self, action):
+    """Request information from GitHub and parse the returned JSON"""
     return json.loads(requests.get(self.request_url(action)).text)
-
-
-
 
