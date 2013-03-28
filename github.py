cf55900a2c07a5267ca0fec9ab4b3572e132485a
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-28 07:32:46 -0500

    rename method to be more explicit

--- github.py
+++ github.py
@@ -18,13 +18,13 @@ class Client:
 
   def repositories(self):
     """Array of Dicts of information about repositores you have access to"""
-    return self.perform_request("user/repos")
+    return self.get("user/repos")
 
   def request_url(self, action):
     """URL to request information from the GitHub API"""
     return "https://api.github.com/%s?access_token=%s" % (action, self.token)
 
-  def perform_request(self, action):
+  def get(self, action):
     """Request information from GitHub and parse the returned JSON"""
     return json.loads(requests.get(self.request_url(action)).text)
 
