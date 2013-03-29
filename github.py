5d4cacc25cc94f0c7338ada261d6d8f53982b41e
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-28 20:53:26 -0500

    add client.org_repositories() method (and support) to get ALL repos

--- github.py
+++ github.py
@@ -16,9 +16,23 @@ class Client:
     """Array of names of repositories you have access to"""
     return [repo['full_name'] for repo in self.repositories()]
 
+  def org_names(self):
+    """Array of names of orgs you have access to"""
+    return [org['login'] for org in self.orgs()]
+
   def repositories(self):
     """Array of Dicts of information about repositores you have access to"""
     return self.get("user/repos")
+
+  def org_repositories(self):
+    org_repos = []
+    for org_name in self.org_names():
+      org_repos += self.get("orgs/%s/repos" % org_name)
+    return org_repos
+
+  def orgs(self):
+    """Array of Dicts of information about organizations you belong to"""
+    return self.get("user/orgs")
 
   def request_url(self, action):
     """URL to request information from the GitHub API"""
